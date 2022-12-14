eb6fb248e42ee16bd1b5aadce83da8bb45ba310b
jeong hyewon <wonizizi89@naver.com>
2022-12-14 15:49:06 +0900

    네이버 검색API 를 이용한 나만의 셀렉트샵  초기 설계 설정

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshop.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
